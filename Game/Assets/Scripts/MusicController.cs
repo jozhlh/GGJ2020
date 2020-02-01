9364ed2070fd3ea5ae6b979dcfbc181738581856
Jack Walker <Jackmw94@gmail.com>
2020-02-01 15:30:47 +0000

    Music + prefab. Listens to MissionStateChanged event

---
+++ Game/Assets/Scripts/MusicController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MusicController : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_music;
+
+    public static Action<bool> MissionStateChanged = (started) => {};
+    void Start()
+    {
+        MissionStateChanged += MissionStateChangedListener;
+    }
+
+    void OnDestroy()
+    {
+        MissionStateChanged -= MissionStateChangedListener;
+    }
+
+    private void MissionStateChangedListener(bool started)
+    {
+        if (started)
+        {
+            m_music.Play();
+        }
+        else
+        {
+            m_music.Stop();
+        }
+    }
+}
